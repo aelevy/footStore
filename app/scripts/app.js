3813b648a4b725f799cc4ac6ea686bf9dd726d85
Andrew Levy <levyandrew44@yahoo.com>
2015-02-11 13:41:51 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name footStoreApp
+ * @description
+ * # footStoreApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('footStoreApp', [
+    'ngRoute',
+    'ngSanitize',
+    'ui.bootstrap'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/homePage.html',
+        controller: 'homePageCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
